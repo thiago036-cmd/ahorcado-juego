60999bd117dfcf37e9e2db6f88b52edbd127eb32
thiago036-cmd <gamerthiago036@gmail.com>
2026-01-10 19:52:40 -0300

    ahorcado.py

--- ahorcado.py
+++ ahorcado.py
@@ -11,7 +11,7 @@ st.markdown("""<style>
     [data-testid="stHorizontalBlock"] { 
         display: grid !important; 
         grid-template-columns: repeat(auto-fit, minmax(80px, 1fr)) !important; /* MÁS ANCHO EN X */
-        gap: 15px !important; 
+        gap: 30px !important; 
     }
     @media (max-width: 600px) { [data-testid="stHorizontalBlock"] { grid-template-columns: repeat(4, 1fr) !important; } }
     
@@ -66,3 +66,4 @@ else:
                 if l.lower() in s.u: st.button("✅" if l.lower() in s.p else "❌", key=l, disabled=True)
                 elif st.button(l, key=l):
                     s.u.append(l.lower()); s.v -= 1 if l.lower() not in s.p else 0; st.rerun()
+
