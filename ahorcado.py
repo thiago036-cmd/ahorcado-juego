702355d28eda0d59ea98909fa12ccb90d2867471
thiago036-cmd <gamerthiago036@gmail.com>
2026-01-10 19:54:52 -0300

    ahorcado.py

--- ahorcado.py
+++ ahorcado.py
@@ -18,8 +18,8 @@ st.markdown("""<style>
     button { 
         background:#1c2128 !important; 
         border:10px solid #000 !important; /* BORDE LATERAL MÁS ANCHO */
-        border-bottom: 400px solid #000 !important; 
-        border-radius:15px !important; 
+        border-bottom: 20px solid #000 !important; 
+        border-radius:30px !important; 
         height:90px !important; width:100% !important;
         padding: 0 0 15px 0 !important;
         display: flex !important; align-items: center !important; justify-content: center !important;
@@ -66,6 +66,7 @@ else:
                 if l.lower() in s.u: st.button("✅" if l.lower() in s.p else "❌", key=l, disabled=True)
                 elif st.button(l, key=l):
                     s.u.append(l.lower()); s.v -= 1 if l.lower() not in s.p else 0; st.rerun()
+
 
 
 
