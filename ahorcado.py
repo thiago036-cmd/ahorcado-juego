d0f20fa5848d37473f66f1f2129855bdaaea04ce
thiago036-cmd <gamerthiago036@gmail.com>
2026-01-11 21:22:31 -0300

    ahorcado.py

--- ahorcado.py
+++ ahorcado.py
@@ -15,8 +15,8 @@ state = get_global_state() # Esto es lo mismo para ti y para tu hermano
 
 st.markdown("""<style>
     .stApp { background:#0e1117; color:white; }
-    [data-testid="stHorizontalBlock"] { display: grid !important; grid-template-columns: repeat(auto-fit, minmax(120px, 1fr)) !important; gap: 8px !important; justify-content: center !important; }
-    button { background:#1c2128 !important; border: none !important; border-radius:8px !important; height:70px !important; }
+    [data-testid="stHorizontalBlock"] { display: grid !important; grid-template-columns: repeat(auto-fit, minmax(60px, 1fr)) !important; gap: 16px !important; justify-content: center !important; }
+    button { background:#1c2128 !important; border: none !important; border-radius:8px !important; height:55px !important; }
     button p { color:white !important; font-weight:800 !important; font-size:20px !important; margin:0 !important; }
     .w { font-size:35px; font-weight:900; letter-spacing:10px; text-align:center; color:#58a6ff; margin:20px 0; font-family:monospace; }
     .copy-box { background: #262730; padding: 10px; border-radius: 5px; border: 1px dashed #58a6ff; margin-bottom: 20px; text-align: center; }
@@ -68,6 +68,7 @@ else:
                     state["u"].append(char)
                     if char not in state["p"]: state["v"] -= 1
                     st.rerun()
+
 
 
 
